421f3a9213f4438010155bb7514f933d60b9adf1
koh suzuki <koh.suzuki1212@gmail.com>
2012-01-17 21:10:54 -0800

    My first program

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class hello {
+    static public void main (String[] args) {
+	System.out.println("hello my name is koh i love being koh lalalalalalalla Hello my name is natalie thanks for reading this useless side not thing poopallalaldjjlejkfjfifiiefeefeffs Hi my name is fat dave and IM FAT!!!!!! ");
+    }
+}
+
